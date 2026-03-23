4b4451a6ce39306296d81a5b6659bbead5aa1274
Jeremy Soo <roarior_jeremysoo@hotmail.com>
2026-03-23 11:59:15 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket"
+}
+
